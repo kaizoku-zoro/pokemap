0310883ee08acaa49e847081eff576f7d4c8d1b4
Nikunj Sharma <nikunj.sharma251@gmail.com>
2021-07-04 22:35:08 +0530

    Add files via upload

---
+++ pokemap/src/InfoEnter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Formik } from 'formik';
+function InfoEnter() {
+    return (
+        <div>
+            <Formik
+            ></Formik>
+        </div>
+    )
+}
+
+export default InfoEnter
